4b4a7e339895a1db3d21dbba80ad953af6dc90ed
CHE Yulin <yche@cse.ust.hk>
2017-05-31 18:51:42 +0800

    refactor

--- src/main/java/io/openmessaging/demo/DataDumper.java
+++ src/main/java/io/openmessaging/demo/DataDumper.java
@@ -117,60 +117,17 @@ public class DataDumper {
                 unmap(topicMappedBuff[i]);
             }
         }
-        dataFile.close();
+
         int finished = numberOfFinished.incrementAndGet();
         //the last producer write out the DataIndex object
         if (finished == numberOfProducer.get()) {
+            dataFile.close();
             ObjectOutputStream oos = new ObjectOutputStream(
                     new FileOutputStream(new File(rootPath + File.separator + "index.bin")));
             oos.writeObject(dataFileIndexer);
             // close the writing.
             oos.close();
         }
-
     }
 }
 
-class DataFileIndexer implements Serializable {
-    public int INIT_TOPIC_NUMBER = 100;
-    public int MAX_MINI_CHUNK_NUMBER_PER_TOPIC = 20;
-    int TOPIC_CHUNK_SIZE = 80 * 1024 * 1024; // 80 MB
-
-    public int topicNumber = 0;
-    public ConcurrentHashMap<String, Integer> topicNameToNumber = new ConcurrentHashMap<>(INIT_TOPIC_NUMBER);
-    public long[] topicOffsets = new long[INIT_TOPIC_NUMBER];
-    public int[] topicMiniChunkNumber = new int[INIT_TOPIC_NUMBER];
-    public int[][] topicMiniChunkLengths = new int[INIT_TOPIC_NUMBER][];
-
-    public long currentGlobalDataOffset = 0;
-    public int currentTopicNumber = 0;
-    public ReentrantLock assignLock = new ReentrantLock();
-
-    public DataFileIndexer() {
-        topicNameToNumber.clear();
-        for (int i = 0; i < INIT_TOPIC_NUMBER; i++) {
-            topicOffsets[i] = 0L;
-            topicMiniChunkNumber[i] = 0;
-            topicMiniChunkLengths[i] = new int[MAX_MINI_CHUNK_NUMBER_PER_TOPIC];
-        }
-
-    }
-
-    void assignNumberToTopic(String topicName) {
-        assignLock.lock();
-        if (!topicNameToNumber.containsKey(topicName)) {
-            topicNameToNumber.put(topicName, currentTopicNumber);
-            topicOffsets[currentTopicNumber] = currentGlobalDataOffset;
-            currentGlobalDataOffset += TOPIC_CHUNK_SIZE;
-            currentTopicNumber++;
-        }
-        assignLock.unlock();
-    }
-
-    int getAssignedTopicNumber(String topicName) {
-        if (!topicNameToNumber.containsKey(topicName)) {
-            assignNumberToTopic(topicName);
-        }
-        return topicNameToNumber.get(topicName);
-    }
-}
---
+++ src/main/java/io/openmessaging/demo/DataFileIndexer.java
@@ -0,0 +1,51 @@
+package io.openmessaging.demo;
+
+import java.io.Serializable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by yche on 5/31/17.
+ */
+class DataFileIndexer implements Serializable {
+    public int INIT_TOPIC_NUMBER = 100;
+    public int MAX_MINI_CHUNK_NUMBER_PER_TOPIC = 20;
+    int TOPIC_CHUNK_SIZE = 80 * 1024 * 1024; // 80 MB
+
+    public int topicNumber = 0;
+    public ConcurrentHashMap<String, Integer> topicNameToNumber = new ConcurrentHashMap<>(INIT_TOPIC_NUMBER);
+    public long[] topicOffsets = new long[INIT_TOPIC_NUMBER];
+    public int[] topicMiniChunkNumber = new int[INIT_TOPIC_NUMBER];
+    public int[][] topicMiniChunkLengths = new int[INIT_TOPIC_NUMBER][];
+
+    public long currentGlobalDataOffset = 0;
+    public int currentTopicNumber = 0;
+    public ReentrantLock assignLock = new ReentrantLock();
+
+    public DataFileIndexer() {
+        topicNameToNumber.clear();
+        for (int i = 0; i < INIT_TOPIC_NUMBER; i++) {
+            topicOffsets[i] = 0L;
+            topicMiniChunkNumber[i] = 0;
+            topicMiniChunkLengths[i] = new int[MAX_MINI_CHUNK_NUMBER_PER_TOPIC];
+        }
+    }
+
+    void assignNumberToTopic(String topicName) {
+        assignLock.lock();
+        if (!topicNameToNumber.containsKey(topicName)) {
+            topicNameToNumber.put(topicName, currentTopicNumber);
+            topicOffsets[currentTopicNumber] = currentGlobalDataOffset;
+            currentGlobalDataOffset += TOPIC_CHUNK_SIZE;
+            currentTopicNumber++;
+        }
+        assignLock.unlock();
+    }
+
+    int getAssignedTopicNumber(String topicName) {
+        if (!topicNameToNumber.containsKey(topicName)) {
+            assignNumberToTopic(topicName);
+        }
+        return topicNameToNumber.get(topicName);
+    }
+}
